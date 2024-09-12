73901825a4a1ae67b09105640724958c96c1f539
Alvaropernett <159142882+Alvaropernett@users.noreply.github.com>
2024-09-12 15:15:32 -0500

    IMC

---
+++ Principal.java
@@ -0,0 +1,11 @@
+public class Principal {
+    public static void main(String[] args) {
+        Usuario usuario1 = new Usuario("Luis Martinez", "Masculino", "1142915424", "luis16@gmail.com");
+        usuario1.mostrarInfo();
+
+        Imc imc1 = new Imc(60, 1.71, usuario1);
+        usuario1.agregarImc(imc1);
+
+        imc1.mostrarImc();
+    }
+}
